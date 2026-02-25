dc623c5a8ca1f5f7a96549586b680820f46bacf5
Damewan Bareh <damewanbareh86@gmail.com>
2026-02-25 19:28:40 +0530

    Improved UI/UX

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import ollama
 import os 
@@ -8,7 +9,7 @@ app=FastAPI()
 
 #templates  
 templates =Jinja2Templates(directory="templates")
-#app.mount("/static",StaticFiles(directory="static"),name="static")
+app.mount("/static",StaticFiles(directory="static"),name="static")
 
 
 #request models
